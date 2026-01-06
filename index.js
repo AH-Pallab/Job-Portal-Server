92bf9fe3675ca42113ac066c5f20cc45f521d8f0
Arif Hossain Pallab <ah.pallab@Arifs-MacBook-Air.local>
2026-01-06 18:56:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iclhmml.mongodb.net/?appName=Cluster0`;
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Job server is ready.");
+});
+
+app.listen(port, () => {
+  console.log(`Job server is running on port: ${port}`);
+});
+
+async function run() {
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  const jobCollections = client.db("jobDob").collection("jobs");
+
+  app.get("/users", async (req, res) => {
+    const cursor = jobCollections.find();
+    const result = await cursor.toArray();
+    res.send(result);
+  });
+}
+run();
